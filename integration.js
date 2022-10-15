2df96fabca6bc0735f348fd986fdec6deef75b94
Luke Karrys <luke@lukekarrys.com>
2022-10-14 18:23:33 -0700

    chore: bump @npmcli/template-oss to 4.6.0

--- integration.js
+++ integration.js
@@ -6,15 +6,14 @@ var excluded = require('./excluded')
 var fs = require('fs')
 var count
 
-
 var configOptions = {
   db: 'https://replicate.npmjs.com/registry',
-  include_docs:true,
-  since:0,
-  sequence:function(seq,cb){
+  include_docs: true,
+  since: 0,
+  sequence: function (seq, cb) {
     setImmediate(cb)
   },
-  concurrency:5
+  concurrency: 5,
 }
 
 var max = 0
@@ -24,49 +23,44 @@ var saved = 0
 var size = 0
 
 var start = Date.now()
-var docs = 0;
+var docs = 0
 
 var countObj = {}
 var versionCountObj = {}
 
-
-changes(function(data,done){
+changes(function (data, done) {
   docs++
   var doc = normalize(data.doc)
   var min = minify(doc)
-  if(doc) {
-
-    //excluded(min,doc,countObj,versionCountObj)
+  if (doc) {
+    // excluded(min,doc,countObj,versionCountObj)
 
     var sdoc = JSON.stringify(doc)
-    var smin = JSON.stringify(min)  
-    var diff = sdoc.length-smin.length 
-    saved += sdoc.length-smin.length
+    var smin = JSON.stringify(min)
+    var diff = sdoc.length - smin.length
+    saved += sdoc.length - smin.length
     size += smin.length
 
-    //console.log(diff+'\t'+saved)
+    // console.log(diff+'\t'+saved)
   }
 
-  if(data.seq >= 997485) {
-
+  if (data.seq >= 997485) {
     console.log('finished')
     report()
 
-    //fs.writeFileSync('./excluded.json',JSON.stringify({doc:countObj,version:versionCountObj},null,'  ')+"\n")
+    // fs.writeFileSync('./excluded.json',JSON.stringify({doc:countObj,version:versionCountObj},null,'  ')+"\n")
     process.exit()
   }
 
   done()
 }, configOptions)
 
+setInterval(report, 5000)
 
-setInterval(report,5000)
-
-function report(){
-
-    console.log('docs: ',docs)
-    console.log('saved: ',saved)
-    console.log('min doc size:',size)
-    var total = Date.now()-start 
-    console.log('elapsed: ',total,"\n")
+function report () {
+  console.log('docs: ', docs)
+  console.log('saved: ', saved)
+  console.log('min doc size:', size)
+  var total = Date.now() - start
+  console.log('elapsed: ', total, '\n')
 }
